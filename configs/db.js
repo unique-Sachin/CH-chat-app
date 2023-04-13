2fdc213f919939abbfac9f5fc19a9b23d265988f
Sachin Mishra <msachin786.pm@gmail.com>
2023-04-13 19:27:56 +0530

    add

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
-    const res = await mongoose.connect(process.env.mongoURL);
+    const res = await mongoose.connect(process.env.mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    });
     console.log(`mongoDB connected to ${res.connection.host}`);
   } catch (error) {
     console.log(`Connection interrupted for ${error}`);
